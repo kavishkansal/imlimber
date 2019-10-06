02b90da5466c1676c6bbb3d1591f7a85ff6c3c71
arishballana <arish.ballana@gmail.com>
2019-10-06 22:45:55 +0530

    Exception Handling
    Completed

--- core/src/main/java/Controller/PersonController.java
+++ core/src/main/java/Controller/PersonController.java
@@ -2,11 +2,7 @@ package Controller;
 
 import POJO.Person;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.server.ResponseStatusException;
-
-import Exceptions.UnAuthorizedException;
 import services.PersonService;
 
 import java.util.ArrayList;
@@ -20,25 +16,14 @@ public class PersonController {
 
     @PostMapping("/persist")
     public void persist(@RequestBody Person person) {
-        try {
-            System.out.println(person.toString());
-            personService.persistPerson(person);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+        System.out.println(person.toString());
+        personService.persistPerson(person);
+
     }
 
     @RequestMapping("/all")
     public ArrayList<Person> getAll() {
-        try{
-        		return personService.getAll();
-        }catch(UnAuthorizedException e){
-        	throw new ResponseStatusException(HttpStatus.UNAUTHORIZED);
-        }catch(Exception e){
-        	throw new ResponseStatusException(HttpStatus.UNAUTHORIZED);
-        	//e.printStackTrace();
-        	//return null;
-        }
+        return personService.getAll();
     }
 
     @RequestMapping("{id}")
--- core/src/main/java/Exceptions/UnAuthorizedException.java
+++ core/src/main/java/Exceptions/UnAuthorizedException.java
@@ -8,4 +8,5 @@ public class UnAuthorizedException extends RuntimeException{
 	public UnAuthorizedException(String message, Throwable cause){
 		super(message,cause);
 	}
+
 }
